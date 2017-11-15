06d4359235380fb5da81c7b76b0a2358ac5f3c84
Arman Singh 👌 <armsingh@amazon.com>
2017-11-15 09:58:48 +0530

    Beta Release 1.0

--- public/assets/js/set-background.js
+++ public/assets/js/set-background.js
@@ -1,3 +1,10 @@
-var idx = Math.floor((new Date().getHours()));
-var body = document.getElementsByTagName("body")[0];
-body.className = "heaven-" + idx;
+function evaluateMe() {
+    var year=document.getElementById("year").value;
+    var month=document.getElementById("mm").value;
+    var day=document.getElementById("dd").value;
+
+    var date = new Date(year, month-1, day, 0, 0, 0, 0);
+    //console.log(date);
+    document.getElementById("mytext").innerHTML = date.getTime();
+    document.getElementById("mytext2").innerHTML = date.getTime()/1000;
+}
